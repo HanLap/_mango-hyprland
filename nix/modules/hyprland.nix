4a0cf21d9db99cce81a6727f5e12a7c862702d2f
Hannah Lappe <hannah@lappe.page>
2022-12-24 08:58:51 +0100

    auto update

--- nix/modules/hyprland.nix
+++ nix/modules/hyprland.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }:
 
 {
+  environment.sessionVariables = rec {
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+    XCURSOR_SIZE = "24";
+    GTK_THEME = "Adwaita-dark";
+  };
+
   environment.systemPackages = with pkgs; [
     waybar
     wofi
