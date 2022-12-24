f0b27c20a07509265f23b08256ebf007b767122a
Hannah Lappe <hannah@lappe.page>
2022-12-24 09:16:37 +0100

    auto update

--- nix/modules/hyprland.nix
+++ nix/modules/hyprland.nix
@@ -1,6 +1,36 @@
 { pkgs, ... }:
 
+let
+
+  dbus-hyprland-environment = pkgs.writeTextFile {
+    name = "dbus-hyprland-environment";
+    destination = "/bin/dbus-hyprland-environment";
+    executable = true;
+
+    text = ''
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=hyprland
+      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+    '';
+  };
+
+  configure-gtk = pkgs.writeTextFile {
+    name = "configure-gtk";
+    destination = "/bin/configure-gtk";
+    executable = true;
+    text = let
+      schema = pkgs.gsettings-desktop-schemas;
+      datadir = "${schema}/share/gesettings/schemas/${schema.name}";
+    in ''
+      export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+      gnome_schema=org.gnome.desktop.interface
+      gesettings set $gnome_schema gtk-theme 'Adwaita-dark'
+    '';
+  };
+in
 {
+
+
   environment.sessionVariables = rec {
     _JAVA_AWT_WM_NONREPARENTING = "1";
     XCURSOR_SIZE = "24";
@@ -8,7 +38,18 @@
   };
 
   environment.systemPackages = with pkgs; [
+    dbus-hyprland-environment
+    configure-gtk
     waybar
     wofi
   ];
+
+
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    # gtk portal needed to make gtk apps happy
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
 }
