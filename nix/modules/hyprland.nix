0d7271be48ebe602c840a6ddb97c2798451d56f9
Hannah Lappe <hannah@lappe.page>
2023-01-23 12:02:08 +0100

    auto update

--- nix/modules/hyprland.nix
+++ nix/modules/hyprland.nix
@@ -42,16 +42,14 @@ in
   environment.sessionVariables = rec {
     _JAVA_AWT_WM_NONREPARENTING = "1";
     XCURSOR_SIZE = "24";
-    GTK_THEME = "Adwaita-dark";
+    GTK_THEME = "Adwaita:dark";
   };
 
   environment.systemPackages = with pkgs; [
-    configure-gtk
     dbus-hyprland-environment
     waybar
     wl-clipboard
     wofi
-    gnome.dconf-editor
   ];
 
   services.dbus.enable = true;
