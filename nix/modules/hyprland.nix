0ec28ab54eb8cf1786a87ca32f3f4719a8687f0b
Hannah Lappe <hannah@lappe.page>
2023-01-30 10:38:08 +0100

    auto update

--- nix/modules/hyprland.nix
+++ nix/modules/hyprland.nix
@@ -42,10 +42,14 @@ in
   environment.sessionVariables = rec {
     _JAVA_AWT_WM_NONREPARENTING = "1";
     XCURSOR_SIZE = "24";
-    GTK_THEME = "Adwaita:dark";
+    GTK_THEME = "Catppuccin-Mocha-Standard-Pink-Dark";
   };
 
   environment.systemPackages = with pkgs; [
+    (catppuccin-gtk.override {
+      accents = ["pink"];
+      variant = "mocha";
+    })
     dbus-hyprland-environment
     socat
     waybar
